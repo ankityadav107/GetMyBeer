e11f92bb54eb9f8881ac6589a010c290eccbdc57
ankityadav107 <ankitrao380@gmail.com>
2022-05-04 15:40:30 -0400

    Add files via upload

---
+++ GetMyBeer/build/precache-manifest.fc288e3e1b82a511d32ae269ff6d11fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c984c8639976cb866f41874363a71c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "721f3f4565f02eb9bce4",
+    "url": "/static/css/main.0220429b.chunk.css"
+  },
+  {
+    "revision": "7d812138a647b78f4a9f",
+    "url": "/static/js/2.54a96250.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.54a96250.chunk.js.LICENSE"
+  },
+  {
+    "revision": "721f3f4565f02eb9bce4",
+    "url": "/static/js/main.c1cddc3f.chunk.js"
+  },
+  {
+    "revision": "bddc15a9d8daafb3da06",
+    "url": "/static/js/runtime-main.44c174db.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
